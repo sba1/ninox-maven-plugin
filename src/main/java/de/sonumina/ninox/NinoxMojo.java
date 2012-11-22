2bddd2327d1a9ee8a0f75c3c2959eb91eb53458f
Sebastian Bauer <mail@sebastianbauer.info>
2012-11-22 17:03:36 +0100

    Depend on newer maven-plugin-plugin. Removed maven-compiler-plugin dependency.

--- src/main/java/de/sonumina/ninox/NinoxMojo.java
+++ src/main/java/de/sonumina/ninox/NinoxMojo.java
@@ -2,12 +2,16 @@ package de.sonumina.ninox;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import owltools.ontologyrelease.OortConfiguration;
 
 /**
  * Packages the ontologies.
  * 
  * @goal compile
  */
+@Mojo(name = "compile")
 public class NinoxMojo extends AbstractMojo
 {
     public void execute() throws MojoExecutionException
