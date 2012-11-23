8d97c00d702b7c8da1a753f51a86868f9df03eda
Sebastian Bauer <mail@sebastianbauer.info>
2012-11-23 13:51:24 +0100

    Added code to see the output of the application of the ninox plugin while the test is performed

--- ninox-maven-plugin-it/src/test/java/de/sonumina/ninox/NinoxMojoTest.java
+++ ninox-maven-plugin-it/src/test/java/de/sonumina/ninox/NinoxMojoTest.java
@@ -1,7 +1,10 @@
 package de.sonumina.ninox;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
@@ -14,6 +17,15 @@ public class NinoxMojoTest//AbstractMojoTestCase
 	public void testCompile() throws VerificationException, IOException
 	{
 		File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/compile" );
+
 		Verifier verifier = new Verifier(testDir.getAbsolutePath());
+		verifier.executeGoal("compile");
+		verifier.resetStreams();
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(testDir.getAbsolutePath(),verifier.getLogFileName()))));
+		String l;
+		while ((l = br.readLine())!= null)
+			System.out.println("NINOXOUT:" + l);
+		br.close();
 	}
 }
