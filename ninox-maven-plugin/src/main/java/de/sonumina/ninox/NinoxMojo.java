2002bb0a7ac249dcf405557c7243a6817449a1c4
Sebastian Bauer <mail@sebastianbauer.info>
2012-11-24 21:42:58 +0100

    Started to implement the application of oort.

--- ninox-maven-plugin/src/main/java/de/sonumina/ninox/NinoxMojo.java
+++ ninox-maven-plugin/src/main/java/de/sonumina/ninox/NinoxMojo.java
@@ -1,9 +1,19 @@
 package de.sonumina.ninox;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.obolibrary.obo2owl.OboInOwlCardinalityTools.AnnotationCardinalityException;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
+import owltools.ontologyrelease.OboOntologyReleaseRunner;
+import owltools.ontologyrelease.OboOntologyReleaseRunnerCheckException;
 import owltools.ontologyrelease.OortConfiguration;
 
 /**
@@ -12,11 +22,49 @@ import owltools.ontologyrelease.OortConfiguration;
 @Mojo(name = "compile")
 public class NinoxMojo extends AbstractMojo
 {
+	@Parameter( defaultValue = "${project.compileSourceRoots}", readonly = true, required = true )
+	private List<String> compileSourceRoots;
+
     public void execute() throws MojoExecutionException
     {
+    	File oboDir = new File(new File(compileSourceRoots.get(0)).getParentFile(),"obo");
+    	String oboSourcePath = oboDir.getAbsolutePath();
+
     	/** Instanciate a new oort config */
 		OortConfiguration oortConfig = new OortConfiguration();
 
-        getLog().info( "Hello, world." );
+		getLog().info("Source roots");
+		for (String sourceRoot : compileSourceRoots)
+			getLog().info(sourceRoot.toString());
+
+		getLog().info("Using " + oboSourcePath);
+
+		
+		for (String file : oboDir.list())
+		{
+			getLog().info("Adding file " + file);
+			oortConfig.addPath(new File(oboDir,file).getAbsolutePath());
+		}
+		
+		try {
+			OboOntologyReleaseRunner oorr = new OboOntologyReleaseRunner(oortConfig, oortConfig.getBase());
+			oorr.createRelease(oortConfig.getPaths());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OWLOntologyCreationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OWLOntologyStorageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OboOntologyReleaseRunnerCheckException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AnnotationCardinalityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
     }
 }
