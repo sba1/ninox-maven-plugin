28c509aadb5f9a757121c6c2c6ff078bd3483391
Sebastian Bauer <mail@sebastianbauer.info>
2012-11-24 20:08:38 +0100

    Finally managed to get the integration tests working.
    It's not perfect yet (slow). Basically, we manually install the ninox-maven-plugin and its parent pom manually into a temp local repo before invoking the goal in the test pom.

--- ninox-maven-plugin-it/src/test/java/de/sonumina/ninox/NinoxMojoTest.java
+++ ninox-maven-plugin-it/src/test/java/de/sonumina/ninox/NinoxMojoTest.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
@@ -13,12 +14,33 @@ import org.junit.Test;
 
 public class NinoxMojoTest//AbstractMojoTestCase
 {
+	private String LOCAL_REPO_PATH = "/tmp/ninox-maven-plugin-repo";
+	
 	@Test
 	public void testCompile() throws VerificationException, IOException
 	{
 		File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/compile" );
+		System.out.println(testDir.getAbsolutePath());
+	
+		/* Install the ninox plugin into the local repo */
+		File mainDir = new File(testDir,"../../../../ninox-maven-plugin");
+		Verifier mainVerifier = new Verifier(mainDir.getAbsolutePath());
+		mainVerifier.setLocalRepo(LOCAL_REPO_PATH);
+		mainVerifier.executeGoal("install");
+		mainVerifier.resetStreams();
 
+		/* Install the ninox parent plugin into the local repo */
+		File pomDir = new File(testDir,"../../../..");
+		Verifier pomVerifier = new Verifier(pomDir.getAbsolutePath());
+		pomVerifier.setLocalRepo(LOCAL_REPO_PATH);
+		pomVerifier.setCliOptions(Arrays.asList("-N"));
+		pomVerifier.executeGoal("install");
+		pomVerifier.resetStreams();
+
+		/* Finally, invoke the goal */
 		Verifier verifier = new Verifier(testDir.getAbsolutePath());
+//		verifier.setCliOptions(Arrays.asList("-o"));
+		verifier.setLocalRepo(LOCAL_REPO_PATH);
 		verifier.executeGoal("compile");
 		verifier.resetStreams();
 	
